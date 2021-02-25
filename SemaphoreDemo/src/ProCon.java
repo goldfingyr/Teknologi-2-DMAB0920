881ae1068cf10c358a0ffe153567ad0aee119ef6
Karsten Jeppesen <karsten@jeppesens.com>
2021-02-25 12:45:56 +0100

    SemaphoreDemo
    Nu med comments

--- SemaphoreDemo/src/ProCon.java
+++ SemaphoreDemo/src/ProCon.java
@@ -1,52 +1,88 @@
-import java.util.concurrent.*;
+// java program to demonstrate 
+// use of semaphores Locks 
+import java.util.concurrent.*; 
 
-class Shared {
-	static int count = 0;
-}
+//A shared resource/class. 
+class Shared 
+{ 
+	static int count = 0; 
+} 
 
-class MyThread extends Thread {
+class MyThread extends Thread 
+{
+	// We need 2 semaphores.
+	// This one will prevent the Producer writing twice
 	Semaphore semPro;
+	// This one will prevent the consumer reading twice
 	Semaphore semCon;
 	String threadName;
 	
-	public MyThread( Semaphore mySemPro, Semaphore mySemCon, String myThreadName ) {
-		super(myThreadName);
-		this.semPro = mySemPro;
+	public MyThread(Semaphore mySemPro, Semaphore mySemCon, String threadName) 
+	{ 
+		super(threadName);
+		// We set the semaphores from the main method
+		this.semPro = mySemPro; 
 		this.semCon = mySemCon;
-		this.threadName = myThreadName;
+		this.threadName = threadName;
 	}
 	
 	@Override
-	public void run() {
-		if ( this.getName().equals("Producer") ) {
-			System.out.println("Producer starting...");
-			for (int i=0; i<5; i++) {
-				try {
+	public void run()
+	{ 
+		
+		// run by thread "Producer" 
+		if(this.getName().equals("Producer")) 
+		{
+			System.out.println("Starting " + threadName);
+			for ( int i=0; i<5; i++)
+			{
+				try
+				{
+					// Wait until we may write (initially we are allowed)
+					// coming back here - we must wait for the "Consumer" to read
 					semPro.acquire();
-					Shared.count++;
-					System.out.println( threadName + " Writes: " + Shared.count);
+					Shared.count++; 
+					System.out.println(threadName + " Writes: " + Shared.count); 
+					// Signal the "Consumer" that a value is ready
 					semCon.release();
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					System.out.println(e);
-					e.printStackTrace();
-				}
-			}
-		} else {
-			System.out.println("Consumer starting...");
-			for (int i=0; i<5; i++) {
-				try {
-					semCon.acquire();
-					System.out.println( threadName + " Reads: " + Shared.count);
-					semPro.release();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		System.out.println(threadName + " Ending");
-	}
+					// Now, allowing a context switch -- if possible. 
+					// for thread Consumer to execute 
+					Thread.sleep(10); 
 
-}
+				}
+				catch (InterruptedException exc)
+				{
+					// Should anything fail - then  we end up here
+					System.out.println(exc); 
+				} 
+			}
+			// And we are done 
+			System.out.println("Ending " + threadName); 
+		} 
+		
+		// run by thread Consumer 
+		else
+		{ 
+			System.out.println("Starting " + threadName); 
+			for (int i=0; i< 5; i++)
+			{
+				try
+				{
+					// Waiting for a value to be present (initially: Wait)
+					semCon.acquire();
+					System.out.println(threadName + " Reads: " + Shared.count);
+					// Signal the "Producer" that the value was read, so the buffer is now cleared
+					semPro.release();
+				}
+				catch (InterruptedException exc)
+				{
+					// Should we fail - here we go
+					System.out.println(exc); 
+				} 
+			}
+			// And we are done 
+			System.out.println("Ending " + threadName);
+		} 
+	} 
+} 
+
--- SemaphoreDemo/src/SemaphoreDemo.java
+++ SemaphoreDemo/src/SemaphoreDemo.java
@@ -1,27 +1,35 @@
-import java.util.concurrent.*;
+// java program to demonstrate 
+// use of semaphores Locks 
+import java.util.concurrent.*; 
 
-public class SemaphoreDemo {
 
-	public static void main(String[] args) {
+// Driver class 
+public class SemaphoreDemo 
+{ 
+	public static void main(String args[]) throws InterruptedException 
+	{ 
+		// creating a Semaphore object 
+		// A 1 means that you are allowed 1 write to start with
 		Semaphore semPro = new Semaphore(1);
+		// A 0 means that you are not allowed to read (as nobody has yet written to the buffer)
 		Semaphore semCon = new Semaphore(0);
 		
-		MyThread mt1 = new MyThread(semPro, semCon, "Producer");
-		MyThread mt2 = new MyThread(semPro, semCon, "Consumer");
+		// creating two threads with name A and B 
+		// Note that thread A will increment the count 
+		// and thread B will decrement the count 
+		MyThread mt1 = new MyThread(semPro, semCon, "Producer"); 
+		MyThread mt2 = new MyThread(semPro, semCon, "Consumer"); 
 		
-		mt1.start();
-		mt2.start();
+		// stating threads A and B 
+		mt1.start(); 
+		mt2.start(); 
 		
-		try {
-			mt1.join();
-			mt2.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// waiting for threads A and B 
+		mt1.join(); 
+		mt2.join(); 
 		
-		System.out.println("Count= " + Shared.count);
-
-	}
-
-}
+		// count will always remain 0 after 
+		// both threads will complete their execution 
+		System.out.println("count: " + Shared.count); 
+	} 
+} 
